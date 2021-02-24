1c2ea0e9ab63c5f1610b27727e217a1fce2a8e7e
AideenM12 <a.malone1289@gmail.com>
2021-02-24 13:58:43 +0000

    if statement updated

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,12 +2,23 @@
     document.getElementById('elephant').src='assets/images/strawberry.jpg';
 }*/
 
-function myFunction(){
+/*function myFunction(){
     var x = document.getElementById('elephant');
-    if(x.src.match('assets/images/strawberry.jpg')){
+    if(x.src.match('assets/images/strawberry (640x640).jpg')){
         x.src = "assets/images/elephant (640x640).jpg";
     }
+   
     else{
-        x.src = "assets/images/strawberry.jpg";
+        x.src = "assets/images/strawberry (640x640).jpg";
+    }
+}*/
+function myFunction(){
+    var x = document.getElementById('elephant');
+    if(x.getAttribute('src') == "assets/images/strawberry (640x640).jpg" ){
+        x.src = "assets/images/elephant (640x640).jpg";
+    }
+   
+    else{
+        x.src = "assets/images/strawberry (640x640).jpg";
     }
 }

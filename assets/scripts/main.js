d483a26ee46b6de37f7acbb1d87e2f61c1ddb556
AideenM12 <a.malone1289@gmail.com>
2021-02-24 13:34:09 +0000

    if statement created

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,13 @@
-function myFunction(){
+/*function myFunction(){
     document.getElementById('elephant').src='assets/images/strawberry.jpg';
+}*/
+
+function myFunction(){
+    var x = document.getElementById('elephant');
+    if(x.src.match('assets/images/strawberry.jpg')){
+        x.src = "assets/images/elephant (640x640).jpg";
+    }
+    else{
+        x.src = "assets/images/strawberry.jpg";
+    }
 }

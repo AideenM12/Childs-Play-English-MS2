99c9ca8fdadbff2a43eeda9c7417016fc4d04f4b
AideenM12 <a.malone1289@gmail.com>
2021-03-04 22:37:15 +0000

    nextQuestion function updated and prevQuestion function created

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,107 +1,14 @@
-/*function myFunction(){
-    document.getElementById('elephant').src='assets/images/strawberry.jpg';
-}*/
-
-/*function myFunction(){
-    var x = document.getElementById('elephant');
-    if(x.src.match('assets/images/strawberry (640x640).jpg')){
-        x.src = "assets/images/elephant (640x640).jpg";
-    }
-   
-    else{
-        x.src = "assets/images/strawberry (640x640).jpg";
-    }
-}*/
-/* This code was found on code project.com (https://www.codeproject.com/Questions/845323/JAVA-SCRIPT-image-image-src-match-issue)*/
-/*function myFunction(){
-    var x = document.getElementById('elephant');
-    if(x.getAttribute('src') == "assets/images/strawberry (640x640).jpg" ){
-        x.src = "assets/images/elephant (640x640).jpg";
-    }
-   
-    else{
-        x.src = "assets/images/strawberry (640x640).jpg";
-    }
-}
-while(images< images.length){
-
-}
-function myQuizQuestions(){
-    var images = []
-}
-
-if(button.onclick === img.src){
-
-}*/
-
-
-/*var questions = [
-  {
-    question: "What is this animal?",
-    answer: [
-        {
-          answerText: "Elephant",
-          correct: true
-        },
-        {
-          answerText: "Dog",
-          correct: false
-        }
-    ]
-  }
-]*/
-// This code was found on Stack overflow;
-/*let images = new Array();
- images[0] = "assets/images/elephant.jpg";
-            images[1] = "assets/images/coolcat.jpg";
-            images[2] = "assets/images/penguin.jpg";
-            images[3] = "assets/images/dog.jpg";
-            images[4] = "assets/images/horse.jpg";
-            images[5] = "assets/images/mouse.jpg";
-            images[6] = "assets/images/duck.jpg";
-            images[7] = "assets/images/turtle.jpg";
-            images[8] = "assets/images/parrot.jpg";
-            images[9] = "assets/images/cow.jpg";
-            images[10] = "assets/images/lion.jpg";
-            images[11] = "assets/images/panda-bear.jpg";
-            images[12] = "assets/images/pig.jpg";
-            images[13] = "assets/images/rabbit.jpg";
-            images[14] = "assets/images/shark.jpg";
-            images[15] = "assets/images/owl.jpg";
-            images[16] = "assets/images/unicorn.jpg";
-            
-            var currentPic = 0;
-            var lastPic = images.length-1;
-            function changeImage()
-            {
-                if (currentPic == lastPic)
-                {
-                    currentPic = 0;
-                    document.getElementById('qImg').src = images[currentPic];
-                }
-                else
-                {
-                    currentPic++;
-                    document.getElementById('qImg').src = images[currentPic];
-                }
-            }
 
 
 
 
-    
-    let images = ["assets/images/coolcat.jpg", "assets/images/Dog (640x640).jpg", "assets/images/elephant (640x640).jpg", "assets/images/lion (640x640).jpg", "assets/images/panda bear (640x640).jpg" ];
-    images.push("assets/images/penguin.jpg");
-
-
-
-    function changeImage(){
+   /* function changeImage(){
      let newPic = [Math.floor(Math.random() * images.length)];
      document.getElementById('elephant').src = newPic.toString();
      
      return images[newPic];
 }
-
+*/
 
 
 
@@ -164,7 +71,7 @@ const opt4 = document.getElementById("opt4");
 let solutions = document.getElementsByClassName("solutions");
 
 const nxtBtn = document.getElementById("nxtBtn");
-
+const prevBtn = document.getElementById("prevBtn");
 
 let questions = [
     {
@@ -223,6 +130,33 @@ function askQuestion(){
     opt4.innerHTML = q.opt4;
 
 }
+function prevQuestion(){
+    if(currentQuestion > 0){
+        askQuestion();
+        --currentQuestion
+        
+    }
+    else{
+        currentQuestion = 0;
+    }
+}
+function nextQuestion(){
+    if( currentQuestion < finalQuestion ){
+        
+        askQuestion();
+        currentQuestion++
+             
+      
+    }
+    else{
+        
+         let gameOver = "GameOver";
+         alert(gameOver);
+
+    }
+    
+}
+
 
 /*function checkSelection(answer){
     if(answer === questions[currentQuestion].correct){
@@ -244,34 +178,43 @@ function askQuestion(){
          alert(gameOver);
      }
 }*/
-let answer = [opt1.innerHTML, opt2.innerHTML, opt3.innerHTML, opt4.innerHTML];
+//let answer = [opt1.innerHTML, opt2.innerHTML, opt3.innerHTML, opt4.innerHTML];
 
 function checkSelection(solutions){
-    if(solutions === questions[currentQuestion].correct && currentQuestion < finalQuestion){
+    if(solutions == questions[currentQuestion].correct && currentQuestion < finalQuestion){
         let congrats = "Well Done!";
         alert(congrats);
         
         currentQuestion++
          askQuestion();
     }
-    else if(solutions !== questions[currentQuestion].correct ){
+    else if(solutions != questions[currentQuestion].correct ){
         let uhOh = "uhOh!";
         alert(uhOh);
        
     }
-    else{
+    else if(currentQuestion == finalQuestion){
          let gameOver = "GameOver";
          alert(gameOver);
      }
+     else{
+         nextQuestion();
+     }
+     
 }
-function nextQuestion(){
+
+
+
+/*function nextQuestion(){
     while( currentQuestion < finalQuestion ){
+        currentQuestion++
         askQuestion();
+    
         
       
       
     }
-}
+}*/
 
 
 beginQuiz.addEventListener("click", startQuiz);

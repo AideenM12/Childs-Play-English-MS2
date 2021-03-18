05acfbd0d498e1f25d12e74e9ed5b923efc9fb16
AideenM12 <a.malone1289@gmail.com>
2021-03-18 20:43:59 +0000

    code tidied, margins and padding fixed

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,40 +1,40 @@
 //This file deals solely with handling interactions with the contact form.
 
-//The sendMail function was taken from Matt Rudge's Code Institute lessons and repurposed for this project
+/*The sendMail function was taken from Matt Rudge's 
+Code Institute lessons and repurposed for this project*/
 
 function sendMail(contactForm) {
-	emailjs.send("service_pjbjzz8", "Child's Play English", {
-		"from_name": contactForm.name.value,
-		"from_email": contactForm.emailaddress.value,
-		"message": contactForm.textarea.value
-	}).then(
-		function response() {
-			console.log("SUCCESS", response);
-			document.getElementById("contactUs").reset();
-		},
-		function error() {
-			console.log("FAILED", error);
-		});
+    emailjs.send("service_pjbjzz8", "Child's Play English", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.textarea.value
+    }).then(
+        function response() {
+            console.log("SUCCESS", response);
+            document.getElementById("contactUs").reset();
+        },
+        function error() {
+            console.log("FAILED", error);
+        });
 
-	return false;
+    return false;
 }
 
-/* The contactUs variable is used to create to the submit event listener to trigger the thankYou function*/
+/* The contactUs variable is used to create to the
+ submit event listener to trigger the thankYou function*/
 
 let contactUs = document.getElementById("contactUs");
 
 contactUs.addEventListener('submit', thankYou);
 
-/* thankYouMsg variable allows and thankYou function allow and thank you response to display if the form has been submitted correctly*/
+/* thankYouMsg variable allows and thankYou function allow
+ and thank you response to display if the form
+  has been submitted correctly*/
 
 let thankYouMsg = document.getElementById("thanks");
 
 function thankYou() {
-	thankYouMsg.style.display = "inline";
-	document.getElementById("thank-you-msg").innerHTML = "Thank you for contacting us, we will be in touch shortly.";
+    thankYouMsg.style.display = "inline";
+    document.getElementById("thank-you-msg").innerHTML =
+        "Thank you for contacting us, we will be in touch shortly.";
 }
-
-
-
-
-
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -33,9 +33,11 @@ let usedQuestions = [];
 /* This function changes the background colour each time a new question is asked
  This code was found on codepen and modified for the needs of the project*/
 function changeBackground() {
-	let colorArray = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff", "#fffffc"]
-	let randColor = Math.floor(Math.random() * colorArray.length);
-	$('body').css("background-color", colorArray[randColor]);
+    let colorArray = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff",
+        "#a0c4ff", "#bdb2ff", "#ffc6ff", "#fffffc"
+    ]
+    let randColor = Math.floor(Math.random() * colorArray.length);
+    $('body').css("background-color", colorArray[randColor]);
 }
 
 
@@ -45,135 +47,141 @@ let questionCount = 0;
 let randomQuestion = 0;
 let finalQuestion = questions.length;
 
-/* This function generates a question from the questions array and also calls the change background function*/
+/* This function generates a question from the questions
+ array and also calls the change background function*/
 
 function askQuestion() {
-	let q = randomQuiz();
-	qImages.innerHTML = "<img src=" + q.imgSrc + ">";
-	question.innerHTML = "<p>" + q.question + "</p>";
-	opt1.innerHTML = q.opt1;
-	opt2.innerHTML = q.opt2;
-	opt3.innerHTML = q.opt3;
-	opt4.innerHTML = q.opt4;
-	changeBackground();
+    let q = randomQuiz();
+    qImages.innerHTML = "<img src=" + q.imgSrc + ">";
+    question.innerHTML = "<p>" + q.question + "</p>";
+    opt1.innerHTML = q.opt1;
+    opt2.innerHTML = q.opt2;
+    opt3.innerHTML = q.opt3;
+    opt4.innerHTML = q.opt4;
+    changeBackground();
 }
 
 
-/*This function allows the user to select the questions from the foodQuestions Array*/
+/*This function allows the user to select the questions
+ from the foodQuestions Array*/
 
 function chooseFoodQuiz() {
-	$(foodQuiz).click();
-	questions = foodQuestions;
-	finalQuestion = foodQuestions.length - 1;
-	startQuiz();
-}
-
-/*This function allows the user to select the questions from the objectQuestions Array*/
-
-function chooseObjectQuiz() {
-	$(objectQuiz).click();
-	questions = objectQuestions;
-	finalQuestion = objectQuestions.length - 1;
+    $(foodQuiz).click();
+    questions = foodQuestions;
+    finalQuestion = foodQuestions.length - 1;
     startQuiz();
 }
 
-/* This function randomises the questions and the if statement prevents the questions from
-being repeated */
+/*This function allows the user to select the questions
+ from the objectQuestions Array*/
+
+function chooseObjectQuiz() {
+    $(objectQuiz).click();
+    questions = objectQuestions;
+    finalQuestion = objectQuestions.length - 1;
+    startQuiz();
+}
+
+/* This function randomises the questions and the if statement
+ prevents the questions from being repeated */
 function randomQuiz() {
-	let randomNum = Math.floor(Math.random() * questions.length);
-	//console.log(randomNum);
-	currentQuestionIndex = randomNum;
+    let randomNum = Math.floor(Math.random() * questions.length);
+
+    currentQuestionIndex = randomNum;
     randomQuestion = questions[randomNum];
     // If randomQuestion is not in usedQuestions, it shows
     // Then, the question is put in the array
 
-	if (!usedQuestions.includes(randomQuestion)) { 
-		//console.log(randomQuestion);
-		usedQuestions.push(randomQuestion); 
-	} else {
-		randomQuiz();
-	}
-	return randomQuestion;
+    if (!usedQuestions.includes(randomQuestion)) {
+
+        usedQuestions.push(randomQuestion);
+    } else {
+        randomQuiz();
+    }
+    return randomQuestion;
 }
 
 /* This function checks the answer of the user and responds
  appropriately based on the conditions of whether the answer 
  is correct, incorrect or whether the quiz has finished*/
 
+//Reload function found on stack overflow
+
 function checkSelection(solutions) {
-	if (solutions == questions[currentQuestionIndex].correct && questionCount < 12) {
-		swal.fire({
-			showClass: {
-				popup: 'animate__animated animate__backInDown'
-			},
-			hideClass: {
-				popup: 'animate__animated animate__backOutRight'
-			},
-			customClass: {
-				background: '#71f909',
-				textColor: '#fffffc',
-			},
-			width: 500,
-			height: 600,
-			text: 'Well Done!',
-			confirmButtonText: 'Cool!',
-			confirmButtonColor: '#009bf5',
-			imageUrl: 'assets/images/misc/littlegirl.jpg',
-			imageWidth: 200,
-			imageHeight: 300,
-			imageAlt: 'Custom image',
-			background: '#71f909',
+    if (solutions == questions[currentQuestionIndex].correct && questionCount <
+        12) {
+        swal.fire({
+            showClass: {
+                popup: 'animate__animated animate__backInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__backOutRight'
+            },
+            customClass: {
+                background: '#71f909',
+                textColor: '#fffffc',
+            },
+            width: 500,
+            height: 600,
+            text: 'Well Done!',
+            confirmButtonText: 'Cool!',
+            confirmButtonColor: '#009bf5',
+            imageUrl: 'assets/images/misc/littlegirl.jpg',
+            imageWidth: 200,
+            imageHeight: 300,
+            imageAlt: 'Custom image',
+            background: '#71f909',
 
-			textColor: '#fffffc'
-		});
-		randomQuestion++;
-		askQuestion();
-		questionCount++;
-		progressCounter();
+            textColor: '#fffffc'
+        });
+        randomQuestion++;
+        askQuestion();
+        questionCount++;
+        progressCounter();
 
-	} else if (solutions != questions[currentQuestionIndex].correct) {
-		swal.fire({
-			showClass: {
-				popup: 'animate__animated animate__jello'
-			},
-			hideClass: {
-				popup: 'animate__animated animate__backOutRight'
-			},
-			textColor: '#fffffc',
-			text: 'Uh Oh Try Again!',
-			confirmButtonText: 'Okay',
-			confirmButtonColor: '#009bf5',
-			imageUrl: 'assets/images/misc/littleboy.jpg',
-			imageWidth: 200,
-			imageHeight: 300,
-			imageAlt: 'Custom image',
-			background: '#d00000',
-		});
-	} else if (questionCount == 12) {
-		swal.fire({
-			showClass: {
-				popup: 'animate__animated animate__swing'
-			},
-			hideClass: {
-				popup: 'animate__animated animate__backOutRight'
-			},
-			textColor: '#fffffc',
-			text: 'Game Over! Great Work!',
-			confirmButtonText: 'Okay',
-			confirmButtonColor: '#d81159',
-			imageUrl: 'assets/images/misc/astronaut.jpg',
-			imageWidth: 200,
-			imageHeight: 300,
-			imageAlt: 'Custom image',
-			background: '#009bf5',
+    } else if (solutions != questions[currentQuestionIndex].correct) {
+        swal.fire({
+            showClass: {
+                popup: 'animate__animated animate__jello'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__backOutRight'
+            },
+            textColor: '#fffffc',
+            text: 'Uh Oh Try Again!',
+            confirmButtonText: 'Okay',
+            confirmButtonColor: '#009bf5',
+            imageUrl: 'assets/images/misc/littleboy.jpg',
+            imageWidth: 200,
+            imageHeight: 300,
+            imageAlt: 'Custom image',
+            background: '#d00000',
+        });
+    } else if (questionCount == 12) {
+        swal.fire({
+            showClass: {
+                popup: 'animate__animated animate__swing'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__backOutRight'
+            },
+            textColor: '#fffffc',
+            text: 'Game Over! Great Work!',
+            confirmButtonText: 'Okay',
+            confirmButtonColor: '#d81159',
+            imageUrl: 'assets/images/misc/astronaut.jpg',
+            imageWidth: 200,
+            imageHeight: 300,
+            imageAlt: 'Custom image',
+            background: '#009bf5',
 
-		}).then(function () {
-			location.reload(); //Reload function found on stack overflow
-		});
+        }).then(function() {
+            location.reload();
+        });
 
-	} else {
-		location.reload();
-	}
+    } else {
+        location.reload();
+    }
 
 }
 
@@ -182,10 +190,12 @@ function checkSelection(solutions) {
 
 function progressCounter() {
 
-	return document.getElementById("counter").innerHTML = `Score: ${questionCount}/12`;
+    return document.getElementById("counter").innerHTML =
+        `Score: ${questionCount}/12`;
 }
 
-/* These event listeners are used to determine the appropriate function based on the users action */
+/* These event listeners are used to determine the appropriate
+ function based on the users action */
 
 beginQuiz.addEventListener("click", startQuiz);
 
@@ -193,17 +203,18 @@ foodQuiz.addEventListener("click", chooseFoodQuiz);
 
 objectQuiz.addEventListener("click", chooseObjectQuiz);
 
-/* This function initiates the quiz based on which event listener has been triggered*/
+/* This function initiates the quiz based on which
+ event listener has been triggered*/
 
 function startQuiz() {
-	beginQuiz.style.display = "none";
-	welcomeQuiz.style.display = "none";
-	titleHeader.style.display = "none";
-	askQuestion();
-	progressCounter();
-	quiz.style.display = "block";
-	question.style.display = "inline";
-	qImages.style.display = "block";
-	solution.style.display = "inline";
-	counter.style.display = "inline";
+    beginQuiz.style.display = "none";
+    welcomeQuiz.style.display = "none";
+    titleHeader.style.display = "none";
+    askQuestion();
+    progressCounter();
+    quiz.style.display = "block";
+    question.style.display = "inline";
+    qImages.style.display = "block";
+    solution.style.display = "inline";
+    counter.style.display = "inline";
 }
